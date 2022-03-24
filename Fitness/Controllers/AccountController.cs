c2a81c9a92021cbf233e987a3a2ec1cdb34c65ab
Иван Иванович <v.aci2000@mail.ru>
2022-03-25 03:15:36 +0500

    Изменения в Layout, в шаблоне страницы + небольшие изменения

--- Fitness/Controllers/AccountController.cs
+++ Fitness/Controllers/AccountController.cs
@@ -11,12 +11,18 @@ namespace Fitness.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -43,6 +49,8 @@ namespace Fitness.Controllers
                 {
                     // установка куки
                     await _signInManager.SignInAsync(user, false);
+
+                    await _userManager.AddToRoleAsync(user, "registeredUser");
                     return RedirectToAction("Index", "Home");
                 }
                 else
--- Fitness/Startup.cs
+++ Fitness/Startup.cs
@@ -63,7 +63,7 @@ namespace Fitness
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
